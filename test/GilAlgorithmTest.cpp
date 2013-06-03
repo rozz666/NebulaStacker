eabe409cb18cfc7e4a0a58af02c0d6a3231f5593
Rafal Przywarski <>
2013-06-03 20:20:04 +0200

    refactored for_each_channel_accumulate_test

--- test/GilAlgorithmTest.cpp
+++ test/GilAlgorithmTest.cpp
@@ -16,8 +16,8 @@ TEST_F(for_each_channel_accumulate_test, should_call_functor_for_each_channel_in
     rgb16_image_t img1(1, 1);
     rgb8_image_t img2(1, 1);
 
-    view(img1)(0, 0) = rgb16_pixel_t(1025, 18, 36);
-    view(img2)(0, 0) = rgb8_pixel_t(1, 2, 4);
+    view(img1)(0, 0) = { 1025, 18, 36 };
+    view(img2)(0, 0) = { 1, 2, 4 };
 
     ASSERT_EQ(1024 + 16 + 32, for_each_channel_accumulate(const_view(img1), const_view(img2), 0, [&](channel16_t p1, channel8_t p2)
     {
@@ -30,10 +30,10 @@ TEST_F(for_each_channel_accumulate_test, should_call_functor_for_each_pixel_in_p
     rgb16_image_t img1(3, 2), img2(3, 2);
     auto v1 = view(img1), v2 = view(img2);
 
-    v1(0, 0) = rgb16_pixel_t(1, 0, 0); v1(1, 0) = rgb16_pixel_t(2, 0, 0);  v1(2, 0) = rgb16_pixel_t(4, 0, 0);
-    v1(0, 1) = rgb16_pixel_t(8, 0, 0); v1(1, 1) = rgb16_pixel_t(16, 0, 0); v1(2, 1) = rgb16_pixel_t(32, 0, 0);
-    v2(0, 0) = rgb16_pixel_t(64, 0, 0);  v2(1, 0) = rgb16_pixel_t(128, 0, 0);  v2(2, 0) = rgb16_pixel_t(256, 0, 0);
-    v2(0, 1) = rgb16_pixel_t(512, 0, 0); v2(1, 1) = rgb16_pixel_t(1024, 0, 0); v2(2, 1) = rgb16_pixel_t(2048, 0, 0);
+    v1(0, 0) = { 1, 0, 0 }; v1(1, 0) = { 2, 0, 0 };  v1(2, 0) = { 4, 0, 0 };
+    v1(0, 1) = { 8, 0, 0 }; v1(1, 1) = { 16, 0, 0 }; v1(2, 1) = { 32, 0, 0 };
+    v2(0, 0) = { 64, 0, 0 };  v2(1, 0) = { 128, 0, 0 };  v2(2, 0) = { 256, 0, 0 };
+    v2(0, 1) = { 512, 0, 0 }; v2(1, 1) = { 1024, 0, 0 }; v2(2, 1) = { 2048, 0, 0 };
 
     ASSERT_EQ(4095, for_each_channel_accumulate(const_view(img1), const_view(img2), 0, [&](channel16_t p1, channel16_t p2)
     {
@@ -45,8 +45,8 @@ TEST_F(for_each_channel_accumulate_test, should_include_the_initial_value)
 {
     rgb16_image_t img1(1, 1), img2(1, 1);
 
-    view(img1)(0, 0) = rgb16_pixel_t(1, 0, 0);
-    view(img2)(0, 0) = rgb16_pixel_t(1, 0, 0);
+    view(img1)(0, 0) = { 1, 0, 0 };
+    view(img2)(0, 0) = { 1, 0, 0 };
 
     ASSERT_EQ(2 + 8, for_each_channel_accumulate(const_view(img1), const_view(img2), 8, [&](channel16_t p1, channel16_t p2)
     {
