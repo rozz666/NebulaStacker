e1d3526c148f8307b9a39abccd7d73f19c6cc38d
Rafal Przywarski <>
2013-06-03 20:33:00 +0200

    simplified FrameFiles::averageDifference

--- test/FrameFiles.cpp
+++ test/FrameFiles.cpp
@@ -1,6 +1,7 @@
 #include "FrameFiles.hpp"
 #include <gtest/gtest.h>
 #include <ImageIO.hpp>
+#include <GilAlgorithm.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/range.hpp>
 
@@ -43,19 +44,11 @@ std::string FrameFiles::writeInputFrame(const RawImage& frame)
 template <typename View1, typename View2>
 double FrameFiles::averageDifference(const View1& view1, const View2& view2)
 {
-    double difference = 0;
-    typedef typename boost::gil::channel_type<View1>::type Channel;
-    for (std::ptrdiff_t y = 0; y < view1.height(); ++y)
+    auto difference = for_each_channel_accumulate(view1, view2, 0.0, [](double ch1, double ch2)
     {
-        typename View1::x_iterator it1 = view1.row_begin(y);
-        typename View2::x_iterator it2 = view2.row_begin(y);
-
-        for (; it1 != view1.row_end(y); ++it1, ++it2)
-        {
-            boost::gil::static_for_each(*it1, *it2, [&](Channel ch1, Channel ch2) { difference += std::abs(double(ch1) - ch2); });
-        }
-    }
-    return difference / (view1.width() * view1.height() * boost::gil::num_channels<View1>::value);
+        return std::abs(ch1 - ch2); 
+    });
+    return difference / (view1.size() * boost::gil::num_channels<View1>::value);
 }
 
 void FrameFiles::expectAverageDifference(RawImage expected, std::string actualFilename, double expectedDifference)
