ec80ac16bc50b81c6b633cbe0644d2f8804dc1ce
Rafal Przywarski <>
2013-06-03 18:36:02 +0200

    implemented FrameFiles::expectAverageDifference

--- test/FrameFiles.cpp
+++ test/FrameFiles.cpp
@@ -40,7 +40,28 @@ std::string FrameFiles::writeInputFrame(const RawImage& frame)
     return filename;
 }
 
-void FrameFiles::expectAverageDifference(RawImage expected, std::string actualFilename, float expectedDifference)
+template <typename View1, typename View2>
+double FrameFiles::averageDifference(const View1& view1, const View2& view2)
 {
-    FAIL() << "not implemented";
+    double difference = 0;
+    typedef typename boost::gil::channel_type<View1>::type Channel;
+    for (std::ptrdiff_t y = 0; y < view1.height(); ++y)
+    {
+        typename View1::x_iterator it1 = view1.row_begin(y);
+        typename View2::x_iterator it2 = view2.row_begin(y);
+
+        for (; it1 != view1.row_end(y); ++it1, ++it2)
+        {
+            boost::gil::static_for_each(*it1, *it2, [&](Channel ch1, Channel ch2) { difference += std::abs(double(ch1) - ch2); });
+        }
+    }
+    return difference / (view1.width() * view1.height() * boost::gil::num_channels<View1>::value);
+}
+
+void FrameFiles::expectAverageDifference(RawImage expected, std::string actualFilename, double expectedDifference)
+{
+    auto actual = readTiffImage(actualFilename);
+
+    ASSERT_EQ(expected.dimensions(), actual.dimensions());
+    ASSERT_LE(averageDifference(const_view(expected), const_view(actual)), expectedDifference);
 }
--- test/FrameFiles.hpp
+++ test/FrameFiles.hpp
@@ -9,10 +9,13 @@ public:
     void writeFrames(RawImages frames, Strings frameFilenames);
     Strings writeFrames(RawImages frames);
     void expectIdenticalImages(std::string expectedFilename, std::string actualFilename);
-    void expectAverageDifference(RawImage expected, std::string actualFilename, float expectedDifference);
+    void expectAverageDifference(RawImage expected, std::string actualFilename, double expectedDifference);
 private:
     AutoremoveFiles filesToRemove;
     unsigned fileIndex;
+
+    template <typename View1, typename View2>
+    double averageDifference(const View1& view1, const View2& view2);
 
     std::string genInputFileName();
     std::string writeInputFrame(const RawImage& frame);
